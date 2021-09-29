283e72b7e79e19df4d288b75139a0eafc7b8ea41
TeaEyes <90041910+ThijsDeR@users.noreply.github.com>
2021-09-29 14:58:52 +0200

    Basic Pong Command

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 const Discord = require("discord.js")
 require("dotenv").config()
-
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS]})
+const clientIntents = new Discord.Intents(32767);
+const client = new Discord.Client({ intents: clientIntents})
+const prefix = '!'
 
 client.once('ready', () => {
     console.log("Online!")
 })
 
+client.on('message', (message) => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(" ");
+    const command = args.shift().toLowerCase();
+
+    switch(command){
+        case 'ping':
+            message.channel.send("Pong");
+            break;
+        default:
+            break;
+    }
+
+})
+
 client.login(process.env.TOKEN)
-
-
-
