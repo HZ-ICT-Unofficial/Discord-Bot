00f9903fff17c9f121e5c00a3be8b96ae1cefe3f
Auxority <gide0013@hz.nl>
2021-09-30 16:20:34 +0200

    added error checking to command loader

--- util/command-loader.js
+++ util/command-loader.js
@@ -4,10 +4,17 @@ require('dotenv').config();
 const commands = {};
 const commandsDirectory = './commands';
 const requireDirectory = '../commands';
-const commandFiles = fs.readdirSync(commandsDirectory).filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync(commandsDirectory).filter((file) => file.endsWith('.js'));
 for (const fileName of commandFiles) {
 	const command = require(`${requireDirectory}/${fileName}`);
     const commandName = fileName.split('.')[0];
+    if (typeof command.run !== 'function') {
+        throw new Error('No run function has been defined');
+    }
+    
+    if (typeof command.info !== 'object') {
+        throw new Error('Invalid command info.');
+    }
     commands[commandName] = command.run;
 }
 
