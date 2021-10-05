013a8d2cd41898dd03320b7a2b1407078d76c0dc
Auxority <gide0013@hz.nl>
2021-10-05 14:31:14 +0200

    added global slash commands

--- register.js
+++ register.js
@@ -4,11 +4,12 @@ const fs = require('fs');
 require('dotenv').config();
 
 const rest = new REST({ version: '9' }).setToken(process.env.BOT_TOKEN);
+const TEST_GUILD_ID = '892730236406472734';
 
 const commands = [];
-const commandFiles = fs.readdirSync(`./commands`).filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync(process.env.COMMANDS_DIR).filter(file => file.endsWith('.js'));
 for (const fileName of commandFiles) {
-	const command = require(`./commands/${fileName}`);
+	const command = require(`${process.env.COMMANDS_DIR}/${fileName}`);
     commands.push(command.info);
 }
 
@@ -17,6 +18,13 @@ for (const fileName of commandFiles) {
 		console.log('Started refreshing application (/) commands.');
 		await rest.put(
 			Routes.applicationCommands(process.env.CLIENT_ID),
+			{
+				body: commands
+			},
+		);
+		
+		await rest.put(
+			Routes.applicationGuildCommands(process.env.CLIENT_ID, TEST_GUILD_ID),
 			{
 				body: commands
 			},
