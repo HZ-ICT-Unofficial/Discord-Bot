f09d031ec64ff8f29ed05b94a27f9f4dc082d3f3
Auxority <gide0013@hz.nl>
2021-10-05 14:20:10 +0200

    made slash commands global, instead of per-guild

--- register.js
+++ register.js
@@ -15,14 +15,12 @@ for (const fileName of commandFiles) {
 (async () => {
 	try {
 		console.log('Started refreshing application (/) commands.');
-
 		await rest.put(
-			Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+			Routes.applicationCommands(process.env.CLIENT_ID),
 			{
 				body: commands
 			},
 		);
-
 		console.log('Successfully reloaded application (/) commands.');
 	} catch (error) {
 		console.error(error);
